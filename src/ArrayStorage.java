d4f70cc480e1836df797b3f91d9b0230ec7abc5f
Максим Погудин <m.pogudin@ProfIT.local>
2021-12-30 12:15:46 +0500

    Add HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,71 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume r : storage) {
+            if (r != null) {
+                if (uuid.equals(r.toString())) {
+                    return r;
+                }
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                storage[i] = null;
+
+                for (int j = i; j < storage.length - i - 1; j++) {
+                    if (storage[j + 1] != null) {
+                        storage[j] = storage[j + 1];
+                        storage[j + 1] = null;
+                    }
+                    else {
+                        break;
+                    }
+                }
+
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return Arrays.copyOfRange(storage, 0, i);
+            }
+        }
         return new Resume[0];
     }
 
     int size() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
         return 0;
     }
 }
